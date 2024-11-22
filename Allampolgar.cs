724a7f9e533677d1b924c1491af03ccc39e17d72
Levente <122294490+Levi-24@users.noreply.github.com>
2024-11-22 11:09:38 +0100

    Add project files.

---
+++ Allampolgar.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bevolkerung
+{
+    class Allampolgar
+    {
+        private int haviNettoJovedelem;
+
+        public int Id { get; set; }
+        public string Nem { get; set; }
+        public int SzuletesiEv { get; set; }
+        public int Eletkor => DateTime.Now.Year - SzuletesiEv;
+        public int Suly { get; set; }
+        public int Magassag { get; set; }
+        public bool Dohanyzik { get; set; }
+        public string Nemzetiseg { get; set; }
+        public string Nepcsoport { get; set; }
+        public string Tartomany { get; set; }
+        public int NettoJovedelem { get; set; }
+        public int HaviNettoJovedelem => NettoJovedelem / 12;
+        public string IskolaiVegzettseg { get; set; }
+        public string PolitikaiNezet { get; set; }
+        public bool AktivSzavazo { get; set; }
+        public int SorFogyasztasEvente { get; set; }
+        public int KrumpliFogyasztasEvente { get; set; }
+
+        public Allampolgar(string sor)
+        {
+            var adatok = sor.Split(';');
+            Id = int.Parse(adatok[0]);
+            Nem = adatok[1];
+            SzuletesiEv = int.Parse(adatok[2]);
+            Suly = int.Parse(adatok[3]);
+            Magassag = int.Parse(adatok[4]);
+            Dohanyzik = adatok[5] == "Igen";
+            Nemzetiseg = adatok[6];
+            Nepcsoport = adatok[7];
+            Tartomany = adatok[8];
+            NettoJovedelem = int.Parse(adatok[9]);
+            IskolaiVegzettseg = adatok[10];
+            PolitikaiNezet = adatok[11];
+            AktivSzavazo = adatok[12] == "Igen";
+            if (adatok[13] == "NA") { SorFogyasztasEvente = 0; }
+            else SorFogyasztasEvente = int.Parse(adatok[13]);
+            if (adatok[14] == "NA") { KrumpliFogyasztasEvente = 0; }
+            else KrumpliFogyasztasEvente = int.Parse(adatok[14]);
+        }
+
+        public string ToString(bool valami)
+        {
+            if (valami)
+            {
+                return $"{Id}\t{Nem}\t{SzuletesiEv}\t{Eletkor}\t{Suly}";
+            }
+            else
+            {
+                return $"{Id}\t{Nemzetiseg}\t{Nepcsoport}\t{Tartomany}\t{NettoJovedelem}";
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}\t{Nem}\t{SzuletesiEv}\t{Eletkor}\t{Suly}\t{Magassag}\t{(Dohanyzik ? "igen" : "nem")}\t{Nemzetiseg}\t{Nepcsoport}\t{Tartomany}\t{NettoJovedelem}\t{HaviNettoJovedelem}\t{IskolaiVegzettseg}\t{PolitikaiNezet}\t{(AktivSzavazo ? "igen" : "nem")}\t{SorFogyasztasEvente}\t{KrumpliFogyasztasEvente}";
+        }
+    }
+   
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+
+namespace Bevolkerung
+{
+    public partial class MainWindow : Window
+    {
+        private List<Allampolgar> Lakossag;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Lakossag = new List<Allampolgar>();
+            using StreamReader sr = new StreamReader(@"../../../src/bevölkerung.txt", Encoding.UTF8);
+            _ = sr.ReadLine();
+            while (!sr.EndOfStream) Lakossag.Add(new Allampolgar(sr.ReadLine()));
+
+            grid.ItemsSource = Lakossag;
+
+            for (int i = 0; i < 40; i++) cmbFeladat.Items.Add($"{i+1}");
+        }
+
+        private void cmbFeladat_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            var methodName = $"Feladat{cmbFeladat.SelectedItem}";
+            var method = GetType().GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Instance);
+            method?.Invoke(this, null);
+        }
+
+        private void Feladat1()
+        {
+            MegoldasMondatos.Content = Math.Round(Lakossag.Average(x => x.NettoJovedelem),2);
+        }
+    }
+}
